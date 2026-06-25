ccb10149c99398f5a6843e527c77891ff6b60eb7
手島 崇 <jugend@hotmail.co.jp>
2026-06-25 16:32:59 +0900

    Auto-update products and catalog

--- products.js
+++ products.js
@@ -1,33 +1,37 @@
 const JUGEND_PRODUCTS = [
   {
     "id": "168285558117765100_test_design",
-    "title": "マグカップ - test_design",
+    "title_en": "Mug - test_design",
+    "title_jp": "マグカップ - test_design",
     "description": "Haende noch jung - Koepfe noch unverdorben. Jugend lenkt die Welt und bringt die Adoleszenz auf den Plan.",
-    "image_url": "https://storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png",
+    "image_url": "https://rlv.zazzle.com/svc/view?pid=168285558117765100&t_image_iid=https%3A//storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png&max_dim=600",
     "zazzle_url": "https://www.zazzle.com/api/create/at-238192102477642780?rf=238192102477642780&ax=LinkProduct&template_pdid=168285558117765100&t_image_iid=https%3A%2F%2Fstorage.googleapis.com%2Fjugend-general-workspace-assets%2Fdesigns%2Ftest_design.png",
     "price": "14.95 USD"
   },
   {
     "id": "235471112223334455_test_design",
-    "title": "Tシャツ - test_design",
+    "title_en": "T-Shirt - test_design",
+    "title_jp": "Tシャツ - test_design",
     "description": "Haende noch jung - Koepfe noch unverdorben. Jugend lenkt die Welt und bringt die Adoleszenz auf den Plan.",
-    "image_url": "https://storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png",
+    "image_url": "https://rlv.zazzle.com/svc/view?pid=235471112223334455&t_image_iid=https%3A//storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png&max_dim=600",
     "zazzle_url": "https://www.zazzle.com/api/create/at-238192102477642780?rf=238192102477642780&ax=LinkProduct&template_pdid=235471112223334455&t_image_iid=https%3A%2F%2Fstorage.googleapis.com%2Fjugend-general-workspace-assets%2Fdesigns%2Ftest_design.png",
     "price": "24.95 USD"
   },
   {
     "id": "144111222333444555_test_design",
-    "title": "マウスパッド - test_design",
+    "title_en": "Mousepad - test_design",
+    "title_jp": "マウスパッド - test_design",
     "description": "Haende noch jung - Koepfe noch unverdorben. Jugend lenkt die Welt und bringt die Adoleszenz auf den Plan.",
-    "image_url": "https://storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png",
+    "image_url": "https://rlv.zazzle.com/svc/view?pid=144111222333444555&t_image_iid=https%3A//storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png&max_dim=600",
     "zazzle_url": "https://www.zazzle.com/api/create/at-238192102477642780?rf=238192102477642780&ax=LinkProduct&template_pdid=144111222333444555&t_image_iid=https%3A%2F%2Fstorage.googleapis.com%2Fjugend-general-workspace-assets%2Fdesigns%2Ftest_design.png",
     "price": "12.95 USD"
   },
   {
     "id": "256111222333444555_test_design",
-    "title": "デスクマット - test_design",
+    "title_en": "Desk Mat - test_design",
+    "title_jp": "デスクマット - test_design",
     "description": "Haende noch jung - Koepfe noch unverdorben. Jugend lenkt die Welt und bringt die Adoleszenz auf den Plan.",
-    "image_url": "https://storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png",
+    "image_url": "https://rlv.zazzle.com/svc/view?pid=256111222333444555&t_image_iid=https%3A//storage.googleapis.com/jugend-general-workspace-assets/designs/test_design.png&max_dim=600",
     "zazzle_url": "https://www.zazzle.com/api/create/at-238192102477642780?rf=238192102477642780&ax=LinkProduct&template_pdid=256111222333444555&t_image_iid=https%3A%2F%2Fstorage.googleapis.com%2Fjugend-general-workspace-assets%2Fdesigns%2Ftest_design.png",
     "price": "29.95 USD"
   }
